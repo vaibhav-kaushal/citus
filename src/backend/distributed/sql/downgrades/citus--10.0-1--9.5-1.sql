ba017ad69b981a331959818db8c1bf96af229a38
Marco Slot <marco.slot@gmail.com>
2020-12-16 20:55:40 +0100

    Add citus_set_coordinator_host

--- src/backend/distributed/sql/downgrades/citus--10.0-1--9.5-1.sql
+++ src/backend/distributed/sql/downgrades/citus--10.0-1--9.5-1.sql
@@ -7,5 +7,6 @@
 
 DROP VIEW public.citus_tables;
 DROP FUNCTION pg_catalog.citus_total_relation_size(regclass,boolean);
+DROP FUNCTION pg_catalog.citus_set_coordinator_host(text,int,noderole,name);
 
 #include "../udfs/citus_total_relation_size/7.0-1.sql"
